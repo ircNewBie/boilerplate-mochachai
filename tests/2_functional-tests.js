343f7adfed98906806b647f5f7101c1a7fa08004
Bong Bungalan <bbungalan@gmail.com>
2022-03-01 19:27:54 +0800

    e2e Testing with Zombie.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -92,15 +92,22 @@ suite('Functional Tests with Zombie.js', function () {
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
           browser.assert.element('span#dates', 1);
-  
           done(); 
         });
+        done();
       });
     });
     // #6
+  
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
 
+      browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        done(); 
+      });
       done();
     });
   });
